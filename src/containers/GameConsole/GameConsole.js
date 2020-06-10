db5eebe21a4c35bf10eea49ae0e7bfb4e514055c
ketanmalik <ketanmalik@gmail.com>
2020-06-10 01:15:08 -0500

    plain-js

--- src/containers/GameConsole/GameConsole.js
+++ src/containers/GameConsole/GameConsole.js
@@ -1,24 +1,14 @@
 import React, { Component } from "react";
+import Board from "../../components/Board/Board";
 import classes from "./GameConsole.module.css";
 
 class GameConsole extends Component {
   render() {
-    const squareLight = <div className={classes.SquareLight}></div>;
-    const squaredark = <div className={classes.SquareDark}></div>;
-
-    let leftPanelDivisions = [...Array(18)].map((e) => Array(20));
-    for (let i = 0; i < 18; i++) {
-      for (let j = 0; j < 20; j++) {
-        j % 2 === 0
-          ? (leftPanelDivisions[i][j] = squareLight)
-          : (leftPanelDivisions[i][j] = squaredark);
-      }
-    }
-
+    const board = new Array(10).fill(<Board boardSize={10} />);
     return (
       <div className={classes.Wrapper}>
         <div className={classes.OuterPanel}>
-          <div className={classes.LeftPanel}>{leftPanelDivisions}</div>
+          <div className={classes.LeftPanel}>{board}</div>
         </div>
       </div>
     );
